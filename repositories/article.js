beedf372b34cae6ab854fc5ca6460d6fe05fb6ff
Abaliyoussef <y.abali_etu@enset-media.ac.ma>
2021-06-19 11:27:15 +0000

    Q.23

--- repositories/article.js
+++ repositories/article.js
@@ -5,6 +5,27 @@ module.exports = {
         return Article.findAll();
     },
 
-    
+    getArticles(offset = 0, limit = 10) {
+        return Article.findAll({offset:parseInt(offset),limit:parseInt(limit)});
+       },
+      
+      getArticle(id) {
+       return Article.findOne({ where:{ id:id } });
+       },
+      addArticle(article) { 
+        return Article.create({
+          title: article.title,
+          content: article.content,
+          published:article.published,
+          
+        })
+      },
+      updateArticle(article) {
+        const Obj=Article.findByPk(article.id);
+        return Obj.update({where : {id:article.id}});
+       },
+      deleteArticle(id) { 
+        return Article.destroy({where:{id:id}});
+      },
       // D'autres m
 }
